76841593a40a858aa25d28e981b80a47f8bdc282
Alexander Govorukhin <alexandergdev20@gmail.com>
2025-10-21 01:31:43 +0500

    Update details view

--- app/src/main/java/com/example/detailslist/characters/data/CharactersApi.kt
+++ app/src/main/java/com/example/detailslist/characters/data/CharactersApi.kt
@@ -2,11 +2,12 @@ package com.example.detailslist.characters.data
 
 import com.example.detailslist.characters.data.model.CharactersListResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CharactersApi {
     @GET("character")
     suspend fun getCharacters(
-        //@Query("orderBy") orderBy: String = CREATE_TIME_KEY
+        @Query("orderBy") orderBy: String = CREATE_TIME_KEY
     ): CharactersListResponse
 
     companion object {
